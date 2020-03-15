3fbae4ea06c87b05edc0e21460b769cbf8e45544
Amr Ezzat <amr.m.ezzat@gmail.com>
2020-03-16 00:57:21 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
 import logo from './logo.svg';
 import './App.css';
 
